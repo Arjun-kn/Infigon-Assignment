43cc72216e4738661b3537408150341ab5eac739
arjun <arjunchauhan0009@gmail.com>
2023-11-19 06:47:38 +0530

    code formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import {BrowserRouter,Routes,Route}  from 'react-router-dom'
-import {ToastContainer} from 'react-toastify'
-import './App.css';
-import Login from './component/Login';
-import User from './component/User';
-import ProtectRoute from './component/ProtectRoute';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import Login from "./component/Login";
+import User from "./component/User";
+import ProtectRoute from "./component/ProtectRoute";
 
 function App() {
   return (
     <div className="App">
-      <ToastContainer/>
+      <ToastContainer />
       <BrowserRouter>
-      <Routes>
-      <Route path="/"  element={<Login/>}/>
-      <Route path="/getuser"  element={<ProtectRoute Component={<User/>}/>}/>
-
-
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/getuser"
+            element={<ProtectRoute Component={<User />} />}
+          />
+        </Routes>
       </BrowserRouter>
-     
     </div>
   );
 }
